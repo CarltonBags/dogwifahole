343c7d41e5d912049bc13a605c38dc0d2ae3c064
John Doe <carltonbags@gmail.com>
2024-11-04 16:37:23 +0100

    buy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import img1 from './assets/1.png'
 import img2 from './assets/2.png'
 import img3 from './assets/3.png'
@@ -6,13 +5,21 @@ import './App.css'
 
 function App() {
 
+const buy = () =>{
+  window.open("https://pump.fun/8fJXNxPpFzGuDyEjCQuvb55Yqc4XDBMpGuDbJKd5pump")
+}
+
+
   return (
-    <div className="w-full">
-      <div>
-        <img src={img1} alt="Image 1" />
-        {/* Add other images as needed */}
-        <img src={img2} alt="Image 2" />
-        <img src={img3} alt="Image 3" />
+    <div className="w-full relative">
+      {/* Positioned absolutely above the first image */}
+      <div className="absolute font-bold text-4xl text-yellow-500 z-10 bg-red-600 w-full">
+        get $ANAL <span className="underline hover:cursor-pointer" onClick={buy}>here!</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <img src={img1} alt="Image 1" className="relative z-0 border-b-2 border-white" />
+        <img src={img2} alt="Image 2" className="relative z-0 border-b-2 border-white" />
+        <img src={img3} alt="Image 3" className="relative z-0 " />
       </div>
     </div>
   )
